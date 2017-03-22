991dc724b750feec59091ee2bb37cea13aa6d98e
Tim Garlick <timgarlick11@gmail.com>
2017-03-21 19:16:38 -0600

    added styling

--- js/actions.js
+++ js/actions.js
@@ -26,3 +26,139 @@ $('.stop').click(function() {
 	mediaStream.stop();
 
 });
+
+
+// Global for recaptcha
+
+
+$(function ($) {
+
+	results_enable = $('button.results');
+	var start = true;
+	var questionNumber = $('.pagination-container button');
+	var form = $('form');
+	var checkboxChecked = 0;
+
+	$("button").prop("disabled", true).css('opacity', '.5');//set all buttons to disabled on page load
+
+	var pageLoad = function() {
+		if (start === true) {
+			$("#industry").show();
+			$('.start').addClass('highlight').prop('disabled', false).css('opacity', '1');
+			$('button.showPrev').prop('disabled', false).css('opacity', '1');
+			$('button.results').prop('disabled', true).css('opacity', '.5');
+		}
+	}
+	pageLoad();
+
+	$('.showNext').click(function(e) {
+		$(this).css("box-shadow", "0 0 0");
+
+		var numberFilter = $(this).parent().data('number');
+		
+		questionNumber.removeClass('highlight');
+		questionNumber.eq(numberFilter).each(function(index, element) {
+			$(this).next().prop('disabled', false).css('opacity', '1').addClass('highlight');
+		});
+		$(this).parent().next().fadeIn(500);
+		$(this).parent().fadeOut(-500);
+
+
+	});
+
+	$('.showPrev').click(function(e) {
+		var numberFilter = $(this).parent().data('number');
+		questionNumber.removeClass('highlight');
+		questionNumber.eq(numberFilter).each(function(index, element) {
+			$(this).prev().addClass('highlight');
+
+		});
+		$(this).parent().prev().fadeIn(500);
+		$(this).parent().fadeOut(-500);
+
+
+
+	});
+
+	$("input:radio").change(function (checkbox) { 
+		$(this).parent('label').siblings('.showNext').prop('disabled', false).css('opacity', '1');
+		var parentChecked = $(this).parent().parent().data('number');
+		var otherChecked = $(this).data('recommendation');
+		if (parentChecked === 1 && otherChecked === "other") {
+			$('.other').fadeIn(500);
+			$(this).parent('label').siblings('.showNext').prop('disabled', true).css('opacity', '.5');
+		} else { $('.other').fadeOut(500);}
+	});
+	$('.other').keyup(function() {
+
+		if( $(this).val() ) {
+			$(this).siblings('.showNext').prop('disabled', false).css('opacity', '1');
+		} else { $(this).siblings('.showNext').prop('disabled', true).css('opacity', '.5');}
+	})
+	$("input.industry:checkbox").change(function (e) { 
+
+		checkBoxDetect($(this));
+
+	});
+	$("input.revenue:checkbox").change(function (e) { 
+
+		checkBoxDetect($(this));
+
+	});
+
+	$("input.plugin-recommendation:checkbox").change(function (e) { 
+
+		checkBoxDetect($(this))
+
+	});
+
+	var checkBoxDetect = function(checkbox) {
+		var checkedState = $('input.' + checkbox[0].classList[1] + ':checkbox:checked').length;
+		var buttonState = checkbox.parent('label').siblings('.showNext');	
+
+		if (checkedState > 0) {
+			buttonState.prop('disabled', !checkedState).css('opacity', '1');	
+		}
+		else {
+			buttonState.prop('disabled', !checkedState).css('opacity', '.5');
+		}
+
+
+	}
+
+	// form validation
+
+	$('input[type="tel"], input[type="number"]').keypress(function(e) {
+		if(e.which >= 48 && e.which <= 57) {
+			return true;
+		}
+		else { return false; }
+	})
+
+
+
+	
+
+	var recommendations = [];
+	form.on('submit', function(e) {
+		if($('form').data('form') === 'roi-form' ) {
+			e.preventDefault();
+			$('.form-container').fadeOut(300);
+			$('.chart-container').fadeIn(450);
+
+			inputValues();
+		}
+		else {
+			$.each(form[0], function(i,el) {
+				if (el.checked === true) {
+					recommendations.push(el.dataset.recommendation);
+				}
+			});
+			localStorage.setItem("user_recommendations", JSON.stringify(recommendations));
+		}
+
+		
+	});
+
+	
+});
